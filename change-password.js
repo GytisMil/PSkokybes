f038daee50f3be3468a60ebfc57f81090443f7da
Laurynas Leišis <lauris.leisis@gmail.com>
2021-10-30 18:15:33 +0300

    Made Clean(-er) code changes

--- change-password.js
+++ change-password.js
@@ -5,38 +5,37 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', verifyToken, async (req, res) =>{
-    const { _id } = req.user;
-    if(req.body.newPassword !== req.body.confirmNewPassword) {
-        return res.status(400).send({'newPasswordConfirm': "New Password and Confirm New Password fields do not match!"});
-    }
+    const user_id = req.user._id;
 
-    let user = await User.findOne({ _id: _id });
+    let user = await User.findOne({ _id: user_id });
     if(!user) {
-        return res.status(400).send({'currentPass':"Authentication error. Try to login again."});
+        return res.status(401).send({'currentPass':"Authentication error. Try to login again."});
     }
-    const validPassword = await bcrypt.compare(req.body.currentPassword, user.password);
-    if (!validPassword) {
-        return res.status(400).send({'currentPass':'Incorrect Current Password.'});
-    }
-    var newObj = {'password': req.body.newPassword};
-    const { error } = validatePassword(newObj);
-    if (error) {
-        return res.status(400).send({'newPassword': error.details[0].message});
-    }
+    compareAndValidatePasswords(req.body.currentPassword, user.password);
     var pass =req.body.newPassword;
     const salt = await bcrypt.genSalt(10);
-    pass = await bcrypt.hash(pass, salt);
-
-     await User.findOneAndUpdate({ _id: _id }, {password: pass}, function(err, result){
-        if(err){
-                res.status(400).send({'error': err});
+    var hashed_password = await bcrypt.hash(pass, salt);
+     await User.findOneAndUpdate({ _id: user_id }, {password: hashed_password}, function(err){
+        if (err) {
+            res.status(400).send({ 'error': err });
         }
-        else{
-            if(user){
-                res.send({'successPassword': "Your password has been successfully changed."});
-            }
+        else {
+            res.status(200).send({ 'successPassword': "Your password has been successfully changed." });
         }
     });
 });
+function compareAndValidatePasswords(currentPassword, userPassword) {
+    const validPassword = await bcrypt.compare(currentPassword, userPassword);
+    var passwordValidationObj = { 'password': req.body.newPassword };
+    
+    if(req.body.newPassword !== req.body.confirmNewPassword) {
+        return res.status(400).send({'newPasswordConfirm': "New Password and Confirm New Password fields do not match!"});
+    }
+    if (!validPassword) {
+        return res.status(400).send({'currentPass':'Incorrect Current Password.'});
+    }
+    const { error } = validatePassword(passwordValidationObj);
 
+    if (error) return res.status(400).send({ 'newPassword': error.details[0].message });
+}
 module.exports = router;
